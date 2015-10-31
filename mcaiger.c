d9e51a603d2d7b29be67c49aebd55d32c36a5a50
Matthias Güdemann <matthias.gudemann@gmail.com>
2015-10-31 09:22:05 +0100

    fail on non-constant latch reset logic

--- mcaiger.c
+++ mcaiger.c
@@ -397,9 +397,9 @@ init (unsigned k)
           else
             picosat_assume (l);
         }
-      /* else */
-      /*   die ("reset of latch %s (%u) is undefined (%u)\n", */
-      /*        model->latches[i].name, i, model->latches[i].reset); */
+      else
+        die ("reset of latch %s (%u) is undefined (%u)\n",
+             model->latches[i].name, i, model->latches[i].reset);
     }
 
   report (2, k, "init");
